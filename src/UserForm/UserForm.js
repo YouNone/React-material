7428f1b97f2d2ad8d8409df5ba5fae4dfe11b687
developer01 <developer01@academydev.loc>
2020-10-07 13:59:47 +0300

    fixed radio button, added datepicker

--- src/UserForm/UserForm.js
+++ src/UserForm/UserForm.js
@@ -1,6 +1,12 @@
-import { Button, Input, Paper, withStyles } from '@material-ui/core';
+import { Button, FormControlLabel, FormLabel, Input, Paper, Radio, RadioGroup, withStyles } from '@material-ui/core';
+import { DatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
 import React from 'react'
 import { Link } from 'react-router-dom';
+// правильный инпут
+import DateFnsUtils from '@date-io/date-fns';
+import locale from "date-fns/locale/ru";
+
+
 
 
 const styles = theme => ({
@@ -13,7 +19,7 @@ const styles = theme => ({
     },
     h1: {
         padding: '15px'
-    }, 
+    },
 
     input: {
         margin: '10px'
@@ -22,37 +28,52 @@ const styles = theme => ({
     form: {
         display: 'flex',
         flexDirection: 'column'
+    },
+    button: {
+        margin: '15px 15px 5px'
     }
 
 });
 
 class UserForm extends React.Component {
 
-    submitHandler = event => {
-        event.preventDefault()
-    }
+    // submitHandler = event => {
+    //     event.preventDefault()
+    // }
+
     constructor(props) {
         super(props);
         this.state = {
             name: '',
-            age: null,
-            code: '',
-            date_birth: null
+            password: '',
+            date_birth: null,
+            selectedDate: new Date()
         };
+        this.onChangeValue = this.onChangeValue.bind(this);
     }
+
     myChangeHandler = (event) => {
         let name = event.target.name;
         let val = event.target.value;
-        if (name === "age") {
-            if (!Number(val)) {
-                alert("Your age must be a number");
-            }
-        }
+        // if (name === "age") {
+        //     if (!Number(val)) {
+        //         alert("Your age must be a number");
+        //     }
+        // }
         this.setState({ [name]: val });
+    }
+
+    onChangeValue(event) {
+        console.log(event.target.value);
+    }
+
+    handleDateChange = date => {
+        this.setState({ selectedDate: date })
     }
 
     render() {
         const { classes } = this.props;
+        const { selectedDate } = this.state;
 
         return (
             <Paper className={classes.root} elevation={3}>
@@ -60,7 +81,7 @@ class UserForm extends React.Component {
                     <h3 className={classes.h1}>Создание пользователя</h3>
                     <form className={classes.form} onSubmit={this.submitHandler} >
                         <div>
-                            <label>Имя:</label>
+                            <FormLabel>Имя:</FormLabel>
                             <Input
                                 className={classes.input}
                                 type='text'
@@ -69,7 +90,14 @@ class UserForm extends React.Component {
                             />
                         </div>
                         <div>
-                            <label>Email:</label>
+                            <FormLabel>Пол:</FormLabel>
+                            <RadioGroup aria-label="gender" name="gender1" onChange={this.onChangeValue}>
+                                <FormControlLabel value="female" control={<Radio />} label="Female" />
+                                <FormControlLabel value="male" control={<Radio />} label="Male" />
+                            </RadioGroup>
+                        </div>
+                        <div>
+                            <FormLabel>Email:</FormLabel>
                             <Input
                                 className={classes.input}
                                 type='text'
@@ -78,30 +106,38 @@ class UserForm extends React.Component {
                             />
                         </div>
                         <div>
-                            <label>Код:</label>
+                            <FormLabel>Пароль:</FormLabel>
                             <Input
                                 className={classes.input}
-                                type='text'
-                                name='code'
+                                type='password'
+                                name='password'
                                 onChange={this.myChangeHandler}
                             />
                         </div>
-
                         <div>
-                            <label>Дата рождения:</label>
-
+                            <FormLabel>Дата рождения:</FormLabel>
+                            <MuiPickersUtilsProvider utils={DateFnsUtils} locale={locale}
+                            >
+                                <div className="pickers">
+                                    <DatePicker
+                                        value={selectedDate}
+                                        onChange={this.handleDateChange}
+                                    />
+                                </div>
+                            </MuiPickersUtilsProvider>
                         </div>
                     </form>
                     <Button
+                        className={classes.button}
                         color="primary"
                         variant="contained"
                     >Сохранить
                     </Button>
                     <Link className="text-link" to="/users">
                         <Button
+                            className={classes.button}
                             color="secondary"
                             variant="contained"
-                            className="add-button"
                         >Назад
                         </Button>
                     </Link>
